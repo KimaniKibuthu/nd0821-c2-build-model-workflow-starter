85a4743534f8e6fe1d65093d1fdeee1b62ffe298
KimaniKibuthu <32309808+KimaniKibuthu@users.noreply.github.com>
2022-01-27 10:55:12 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from typing import Union, List
+from pydantic import BaseModel
+
+class TaggedItem(BaseModel):
+    name:str
+    tags:Union[str, List[str]]
+    item_id:int
+
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item:TaggedItem):
+    return item
+
+@app.get("/items/{item_id")
+async def get_items(item_id:int, count:int = 1):
+    return {"fetch":f"Fetched {count} of {item_id}."}
